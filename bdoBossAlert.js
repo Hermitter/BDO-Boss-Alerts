17f3b08bc402046b1f875d2dd4da5d571a8c010d
Carlos Chacin <chacin.carlos9798@gmail.com>
2018-01-30 01:31:58 -0500

    Code indent

--- bdoBossAlert.js
+++ bdoBossAlert.js
@@ -22,94 +22,94 @@ var bossData = {
 var lastSpawn = "";
 var nextSpawn = "";
 function retrieveBossData(callback){
-            //Url for boss spawn data
-			var url = "http://urzasarchives.com/bdo/wbtbdo/wbtna/";
-            //Obtain Boss Spawn Data
-			request(url, function (error, response, body) {
-			    	var $ = cheerio.load(body);
-			    	//Attempt Obtain All And Assign Boss Spawn Data (really messy)
-			    	try{
-                        //////////////////////
-                        //kutum
-                        lastSpawn = $("table").find("tbody > tr").next().find("td").next().next().html();
-                        nextSpawn = $("table").find("tbody > tr").next().next().find("td").next().html();
-                        if(bossData.kutum.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.kutum.lastSpawn)//check if spawned
-                            emitter.bossEvents.sendEvent("bossSpawn", bossData.kutum);//alert if spawned
-                        bossData.kutum.lastSpawn = lastSpawn;
-                        bossData.kutum.nextSpawn = nextSpawn;
+    //Url for boss spawn data
+    var url = "http://urzasarchives.com/bdo/wbtbdo/wbtna/";
+    //Obtain Boss Spawn Data
+    request(url, function (error, response, body) {
+            var $ = cheerio.load(body);
+            //Attempt Obtain All And Assign Boss Spawn Data (really messy)
+            try{
+                //////////////////////
+                //kutum
+                lastSpawn = $("table").find("tbody > tr").next().find("td").next().next().html();
+                nextSpawn = $("table").find("tbody > tr").next().next().find("td").next().html();
+                if(bossData.kutum.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.kutum.lastSpawn)//check if spawned
+                    emitter.bossEvents.sendEvent("bossSpawn", bossData.kutum);//alert if spawned
+                bossData.kutum.lastSpawn = lastSpawn;
+                bossData.kutum.nextSpawn = nextSpawn;
 
-                        //////////////////////
-                        //Dastard Bheg
-                        lastSpawn = $("table").next().find("tbody > tr").next().find("td").next().next().html();
-                        nextSpawn = $("table").next().find("tbody > tr").next().next().find("td").next().html();
-                        if(bossData.dastardBheg.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.dastardBheg.lastSpawn)//check if spawned
-                            emitter.bossEvents.sendEvent("bossSpawn", bossData.dastardBheg);//alert if spawned
-                        bossData.dastardBheg.lastSpawn = lastSpawn;
-                        bossData.dastardBheg.nextSpawn = nextSpawn;
+                //////////////////////
+                //Dastard Bheg
+                lastSpawn = $("table").next().find("tbody > tr").next().find("td").next().next().html();
+                nextSpawn = $("table").next().find("tbody > tr").next().next().find("td").next().html();
+                if(bossData.dastardBheg.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.dastardBheg.lastSpawn)//check if spawned
+                    emitter.bossEvents.sendEvent("bossSpawn", bossData.dastardBheg);//alert if spawned
+                bossData.dastardBheg.lastSpawn = lastSpawn;
+                bossData.dastardBheg.nextSpawn = nextSpawn;
 
-                        //////////////////////
-                        //Dim Tree Spirit
-                        lastSpawn = $("table").next().next().find("tbody > tr").next().find("td").next().next().html();
-                        nextSpawn = $("table").next().next().find("tbody > tr").next().next().find("td").next().html();
-                        if(bossData.dimTreeSpirit.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.dimTreeSpirit.lastSpawn)//check if spawned
-                            emitter.bossEvents.sendEvent("bossSpawn", bossData.dimTreeSpirit);//alert if spawned
-                        bossData.dimTreeSpirit.lastSpawn = lastSpawn;
-                        bossData.dimTreeSpirit.nextSpawn = nextSpawn;
+                //////////////////////
+                //Dim Tree Spirit
+                lastSpawn = $("table").next().next().find("tbody > tr").next().find("td").next().next().html();
+                nextSpawn = $("table").next().next().find("tbody > tr").next().next().find("td").next().html();
+                if(bossData.dimTreeSpirit.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.dimTreeSpirit.lastSpawn)//check if spawned
+                    emitter.bossEvents.sendEvent("bossSpawn", bossData.dimTreeSpirit);//alert if spawned
+                bossData.dimTreeSpirit.lastSpawn = lastSpawn;
+                bossData.dimTreeSpirit.nextSpawn = nextSpawn;
 
-                        //////////////////////
-                        //Giant Mudster
-                        lastSpawn = $("table").next().next().next().find("tbody > tr").next().find("td").next().next().html();
-                        nextSpawn = $("table").next().next().next().find("tbody > tr").next().next().find("td").next().html();
-                        if(bossData.giantMudster.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.giantMudster.lastSpawn)//check if spawned
-                            emitter.bossEvents.sendEvent("bossSpawn", bossData.giantMudster);//alert if spawned
-                        bossData.giantMudster.lastSpawn = lastSpawn;
-                        bossData.giantMudster.nextSpawn = nextSpawn;
+                //////////////////////
+                //Giant Mudster
+                lastSpawn = $("table").next().next().next().find("tbody > tr").next().find("td").next().next().html();
+                nextSpawn = $("table").next().next().next().find("tbody > tr").next().next().find("td").next().html();
+                if(bossData.giantMudster.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.giantMudster.lastSpawn)//check if spawned
+                    emitter.bossEvents.sendEvent("bossSpawn", bossData.giantMudster);//alert if spawned
+                bossData.giantMudster.lastSpawn = lastSpawn;
+                bossData.giantMudster.nextSpawn = nextSpawn;
 
-                        //////////////////////
-                        //Karanda
-                        lastSpawn = $("table").next().next().next().next().find("tbody > tr").next().find("td").next().next().html();
-                        nextSpawn = $("table").next().next().next().next().find("tbody > tr").next().next().find("td").next().html();
-                        if(bossData.karanda.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.karanda.lastSpawn)//check if spawned
-                            emitter.bossEvents.sendEvent("bossSpawn", bossData.karanda);//alert if spawned
-                        bossData.karanda.lastSpawn = lastSpawn;
-                        bossData.karanda.nextSpawn = nextSpawn;
+                //////////////////////
+                //Karanda
+                lastSpawn = $("table").next().next().next().next().find("tbody > tr").next().find("td").next().next().html();
+                nextSpawn = $("table").next().next().next().next().find("tbody > tr").next().next().find("td").next().html();
+                if(bossData.karanda.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.karanda.lastSpawn)//check if spawned
+                    emitter.bossEvents.sendEvent("bossSpawn", bossData.karanda);//alert if spawned
+                bossData.karanda.lastSpawn = lastSpawn;
+                bossData.karanda.nextSpawn = nextSpawn;
 
-                        //////////////////////
-                        //Kzarka
-                        lastSpawn = $("table").next().next().next().next().next().find("tbody > tr").next().find("td").next().next().html();
-                        nextSpawn = $("table").next().next().next().next().next().find("tbody > tr").next().next().find("td").next().html();
-                        if(bossData.kzarka.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.kzarka.lastSpawn)//check if spawned
-                            emitter.bossEvents.sendEvent("bossSpawn", bossData.kzarka);//alert if spawned
-                        bossData.kzarka.lastSpawn = lastSpawn;
-                        bossData.kzarka.nextSpawn = nextSpawn;
+                //////////////////////
+                //Kzarka
+                lastSpawn = $("table").next().next().next().next().next().find("tbody > tr").next().find("td").next().next().html();
+                nextSpawn = $("table").next().next().next().next().next().find("tbody > tr").next().next().find("td").next().html();
+                if(bossData.kzarka.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.kzarka.lastSpawn)//check if spawned
+                    emitter.bossEvents.sendEvent("bossSpawn", bossData.kzarka);//alert if spawned
+                bossData.kzarka.lastSpawn = lastSpawn;
+                bossData.kzarka.nextSpawn = nextSpawn;
 
-                        //////////////////////
-                        //Red Nose
-                        lastSpawn = $("table").next().next().next().next().next().next().find("tbody > tr").next().find("td").next().next().html();
-                        nextSpawn = $("table").next().next().next().next().next().next().find("tbody > tr").next().next().find("td").next().html();
-                        if(bossData.redNose.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.redNose.lastSpawn)//check if spawned
-                            emitter.bossEvents.sendEvent("bossSpawn", bossData.redNose);//alert if spawned
-                        bossData.redNose.lastSpawn = lastSpawn;
-                        bossData.redNose.nextSpawn = nextSpawn;
+                //////////////////////
+                //Red Nose
+                lastSpawn = $("table").next().next().next().next().next().next().find("tbody > tr").next().find("td").next().next().html();
+                nextSpawn = $("table").next().next().next().next().next().next().find("tbody > tr").next().next().find("td").next().html();
+                if(bossData.redNose.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.redNose.lastSpawn)//check if spawned
+                    emitter.bossEvents.sendEvent("bossSpawn", bossData.redNose);//alert if spawned
+                bossData.redNose.lastSpawn = lastSpawn;
+                bossData.redNose.nextSpawn = nextSpawn;
 
-                        //////////////////////
-                        //Nouver
-                        lastSpawn = $("table").next().next().next().next().next().next().next().find("tbody > tr").next().find("td").next().next().html();
-                        nextSpawn = $("table").next().next().next().next().next().next().next().find("tbody > tr").next().next().find("td").next().html();
-                        if(bossData.nouver.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.nouver.lastSpawn)//check if spawned
-                            emitter.bossEvents.sendEvent("bossSpawn", bossData.nouver);//alert if spawned
-                        bossData.nouver.lastSpawn = lastSpawn;
-                        bossData.nouver.nextSpawn = nextSpawn;
+                //////////////////////
+                //Nouver
+                lastSpawn = $("table").next().next().next().next().next().next().next().find("tbody > tr").next().find("td").next().next().html();
+                nextSpawn = $("table").next().next().next().next().next().next().next().find("tbody > tr").next().next().find("td").next().html();
+                if(bossData.nouver.lastSpawn != null && lastSpawn != null && lastSpawn !== bossData.nouver.lastSpawn)//check if spawned
+                    emitter.bossEvents.sendEvent("bossSpawn", bossData.nouver);//alert if spawned
+                bossData.nouver.lastSpawn = lastSpawn;
+                bossData.nouver.nextSpawn = nextSpawn;
 
-                        //give new data to callback
-                        callback(bossData);
-			    	}
-					catch(htmlError){
-                        console.log(htmlError+"\nCould not find HTML element!"+"\n Retrying Now!... ");
-                        //try again in 3 seconds
-                        //setTimeout(retrieveBossData, 3000);
-					}
-			});
+                //give new data to callback
+                callback(bossData);
+            }
+            catch(htmlError){
+                console.log(htmlError+"\nCould not find HTML element!"+"\n Retrying Now!... ");
+                //try again in 3 seconds
+                //setTimeout(retrieveBossData, 3000);
+            }
+    });
 };
 
 ///////////////////////////////////////
